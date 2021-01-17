c88546b4b1c1852668bcac3a31222cf164702721
Suh Donghwi <hwidongsuh@gmail.com>
2021-01-17 15:09:22 +0900

    feat: add misconceptions menu

--- src/pages/MenuPage.tsx
+++ src/pages/MenuPage.tsx
@@ -5,6 +5,7 @@ import Menu from "../components/Menu";
 
 import menu1 from "../lottie/menu1.json";
 import menu2 from "../lottie/menu2.json";
+import menu3 from "../lottie/menu3.json";
 
 const Container = styled.section`
   height: 100vh;
@@ -18,7 +19,8 @@ export default function MenuPage() {
   return (
     <Container>
       <Menu index={1} name="Basics" data={menu1} />
-      <Menu index={2} name="Principal" data={menu2} />
+      <Menu index={2} name="Principals" data={menu2} />
+      <Menu index={3} name="Misconceptions" data={menu3} />
     </Container>
   );
 }
