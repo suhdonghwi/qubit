53f09b09a8522e6552141862d62fd6200c0a2ff8
Suh Donghwi <hwidongsuh@gmail.com>
2021-01-28 17:09:51 +0900

    chore: change geometry to buffergeometry

--- src/components/graphics/basics/slits/ProbCurve.tsx
+++ src/components/graphics/basics/slits/ProbCurve.tsx
@@ -10,7 +10,9 @@ interface ProbCurveProps {
 export default function ProbCurve({ points, opacity }: ProbCurveProps) {
   const curve = new THREE.CatmullRomCurve3(points);
 
-  const tube = new THREE.TubeGeometry(curve, undefined, 0.1);
+  const tube = new THREE.BufferGeometry().fromGeometry(
+    new THREE.TubeGeometry(curve, undefined, 0.1)
+  );
 
   return (
     <mesh geometry={tube} position={[0, 1, 0]}>
