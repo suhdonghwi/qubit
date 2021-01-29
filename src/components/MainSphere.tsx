c677765ddefbe9001140f2744b2abee37eddfebb
Suh Donghwi <hwidongsuh@gmail.com>
2021-01-30 03:55:17 +0900

    feat: add landing page scroll effect

--- src/components/MainSphere.tsx
+++ src/components/MainSphere.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components/macro";
 
 import { useSpring, animated } from "@react-spring/three";
@@ -12,6 +12,17 @@ const StyledCanvas = styled(Canvas)`
 `;
 
 export default function MainSphere() {
+  const [scroll, setScroll] = useState(document.body.scrollTop);
+
+  function onScroll() {
+    setScroll(document.documentElement.scrollTop);
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", onScroll);
+    return () => window.removeEventListener("scroll", onScroll);
+  });
+
   const { torusPos, cylinderPos, boxPos } = useSpring<{
     boxPos: any;
     torusPos: any;
@@ -21,9 +32,9 @@ export default function MainSphere() {
       tension: 100,
     },
     from: {
-      boxPos: [-1.5, -4, 0],
-      torusPos: [-1.2, 4, 0],
-      cylinderPos: [1.5, -2, 0],
+      boxPos: [-1.5, -8, 0],
+      torusPos: [-1.2, 6, 0],
+      cylinderPos: [1.5, -5, 0],
     },
     to: async (next) => {
       await next({
@@ -55,7 +66,7 @@ export default function MainSphere() {
 
       <animated.mesh
         position={boxPos}
-        rotation={[Math.PI / 4, Math.PI / 4, Math.PI / 3]}
+        rotation={[Math.PI / 4, scroll / 200, Math.PI / 3]}
       >
         <boxBufferGeometry args={[1.7, 1.7, 1.7]} />
         <meshLambertMaterial color="#495057" />
@@ -63,7 +74,7 @@ export default function MainSphere() {
 
       <animated.mesh
         position={torusPos}
-        rotation={[Math.PI / 4, Math.PI / 4, 0]}
+        rotation={[Math.PI / 4 + scroll / 150, Math.PI / 4, 0]}
       >
         <torusBufferGeometry args={[1.2, 0.4, 32, 100]} />
         <meshLambertMaterial color="#495057" />
@@ -71,7 +82,7 @@ export default function MainSphere() {
 
       <animated.mesh
         position={cylinderPos}
-        rotation={[Math.PI / 4, 0, Math.PI / 6]}
+        rotation={[Math.PI / 4, 0, Math.PI / 6 + scroll / 300]}
       >
         <cylinderGeometry args={[1.3, 1.3, 3, 100]} />
         <meshLambertMaterial color="#495057" />
