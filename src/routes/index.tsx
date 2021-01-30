a39f771c458c4d3076fdf81d6727a4e090a80778
Suh Donghwi <hwidongsuh@gmail.com>
2021-01-30 18:07:34 +0900

    chore: rename BasicPage to Basic1Page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,14 @@
 import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 
 import MainPage from "../pages/MainPage";
-import BasicPage from "../pages/BasicPage";
+import Basic1Page from "../pages/Basic1Page";
 
 export default function Root() {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={MainPage} />
-        <Route path="/basic/1" exact component={BasicPage} />
+        <Route path="/basic/1" exact component={Basic1Page} />
         <Redirect path="*" to="/" />
       </Switch>
     </BrowserRouter>
