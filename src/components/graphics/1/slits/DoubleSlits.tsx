0d24abc3360377d068c788cd611494b53b408420
Suh Donghwi <hwidongsuh@gmail.com>
2021-02-01 19:39:36 +0900

    feat: make double slits and projection screen receive shadow

--- src/components/graphics/1/slits/DoubleSlits.tsx
+++ src/components/graphics/1/slits/DoubleSlits.tsx
@@ -4,17 +4,17 @@ import { animated, AnimatedProps } from "@react-spring/three";
 export default function DoubleSlits(props: AnimatedProps<GroupProps>) {
   return (
     <animated.group {...props}>
-      <mesh position={[-2.8, 0, 0]} castShadow>
+      <mesh position={[-2.8, 0, 0]} castShadow receiveShadow>
         <boxBufferGeometry args={[3.4, 3, 0.15]} />
         <meshLambertMaterial color="#ced4da" />
       </mesh>
 
-      <mesh position={[2.8, 0, 0]} castShadow>
+      <mesh position={[2.8, 0, 0]} castShadow receiveShadow>
         <boxBufferGeometry args={[3.4, 3, 0.15]} />
         <meshLambertMaterial color="#ced4da" />
       </mesh>
 
-      <mesh position={[0, 0, 0]} castShadow>
+      <mesh position={[0, 0, 0]} castShadow receiveShadow>
         <boxBufferGeometry args={[1.5, 3, 0.15]} />
         <meshLambertMaterial color="#ced4da" />
       </mesh>
--- src/components/graphics/1/slits/ProjectionScreen.tsx
+++ src/components/graphics/1/slits/ProjectionScreen.tsx
@@ -6,7 +6,7 @@ interface ProjectionScreenProps {
 
 export default function ProjectionScreen({ opacity }: ProjectionScreenProps) {
   return (
-    <mesh position={[0, -0.5, -4.35]}>
+    <mesh position={[0, -0.5, -4.35]} receiveShadow>
       <boxBufferGeometry args={[9, 5, 0.3]} />
       <animated.meshLambertMaterial
         color="#ced4da"
