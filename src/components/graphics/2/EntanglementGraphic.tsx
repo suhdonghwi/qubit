124f2d16f8ef7063a6886c18c35221cc2e2b2423
Suh Donghwi <hwidongsuh@gmail.com>
2021-02-01 18:48:53 +0900

    feat: add quantum entanglement graphic

--- src/components/graphics/2/EntanglementGraphic.tsx
+++ src/components/graphics/2/EntanglementGraphic.tsx
@@ -8,15 +8,16 @@ import Button from "../Button";
 
 export default function EntanglementGraphic() {
   const [clicked, setClicked] = useState(false);
-  const probability = clicked ? Math.round(Math.random()) : 0.5;
+  const probability1 = clicked ? Math.round(Math.random()) : 0.5;
+  const probability2 = probability1 === 1 ? 0 : probability1 === 0 ? 1 : 0.5;
 
   return (
     <>
-      <mesh position={[-4, -0.5, 0]} rotation={[0, 0, 0]} castShadow>
+      <mesh position={[-4, -0.5, -1.5]} rotation={[0, 0, 0]} castShadow>
         <boxBufferGeometry args={[0.7, 0.5, 0.5]} />
         <meshLambertMaterial color="#495057" />
       </mesh>
-      <mesh position={[-2, -0.5, 0]} rotation={[0, 0, -Math.PI / 2]}>
+      <mesh position={[-2, -0.5, -1.5]} rotation={[0, 0, -Math.PI / 2]}>
         <cylinderBufferGeometry args={[0.45, 0.1, 4, 32]} />
         <animated.meshLambertMaterial
           color="#ffd43b"
@@ -25,13 +26,21 @@ export default function EntanglementGraphic() {
         />
       </mesh>
       <Button
-        position={[0, -2.75, 2]}
+        position={[0, -2.75, 3]}
         onDown={() => setClicked(true)}
         onUp={() => setClicked(false)}
         click={clicked}
       />
-      <Qubit oneProbability={probability} position={[0, -0.5, -1.5]} size={0.5} />
-      <Qubit oneProbability={probability} position={[0, -0.5, 1.5]} size={0.5} />
+      <Qubit
+        oneProbability={probability1}
+        position={[0, -0.5, -1.5]}
+        size={0.5}
+      />
+      <Qubit
+        oneProbability={probability2}
+        position={[0, -0.5, 1.5]}
+        size={0.5}
+      />
       <Plane />;
     </>
   );
