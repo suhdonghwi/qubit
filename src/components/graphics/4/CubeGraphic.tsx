382959ba7a554db9e28d1aff421b610e35b05185
Suh Donghwi <hwidongsuh@gmail.com>
2021-02-08 23:36:38 +0900

    fix: add suspense fallback to cube graphic

--- src/components/graphics/4/CubeGraphic.tsx
+++ src/components/graphics/4/CubeGraphic.tsx
@@ -1,10 +1,13 @@
+import { Suspense } from "react";
 import Cube from "../models/CubeModel";
 import Plane from "../Plane";
 
 export default function CubeGraphic() {
   return (
     <>
-      <Cube position={[0, 1, 0]} scale={[9, 9, 9]} />
+      <Suspense fallback={null}>
+        <Cube position={[0, 1, 0]} scale={[9, 9, 9]} />
+      </Suspense>
       <Plane />
     </>
   );
